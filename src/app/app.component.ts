737083d9784d5aceb0a4f5247e9cc6470399f462
Minh Luong Van <luongvanminh@huflit.edu.vn>
2019-03-09 12:09:34 +0700

    change project1 lab6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+  loadedFeature = 'recipe';
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
-  }
-
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
